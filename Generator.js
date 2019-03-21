62ad51d039b0c1d9cc4ab839b87b4191f9a0f8ee
Cody <38409768+Cody4687@users.noreply.github.com>
2019-03-21 14:11:27 -0500

    Create Generator.js
    The generator

---
+++ Generator.js
@@ -0,0 +1,12 @@
+function Generate() {
+var mathA = Math.floor(Math.random() * 10)
+var mathB = Math.floor(Math.random() * 10)
+var mathC = Math.floor(Math.random() * 10)
+var mathD = Math.floor(Math.random() * 10)
+var mathE = Math.floor(Math.random() * 10)
+return(`0011` + mathA + mathB + mathC + mathD + mathE)
+}
+
+/*
+Use the function
+*/
